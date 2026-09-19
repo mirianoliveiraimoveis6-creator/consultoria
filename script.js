6dccc0b1b40e3f48e424afdfc385ce73ea2373e2
mirianoliveiraimoveis6-creator <mirian.oliveira.imoveis6@gmail.com>
2026-09-19 17:27:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ async function carregarImoveis() {
 
         destaques.forEach(imovel => {
             const fotoCapa = imovel.Fotos?.find(foto => String(foto.Tipo).trim().toLowerCase() === 'capa') || imovel.Fotos?.[0];
-            const imagemImovel = fotoCapa?.Url || 'assets/images/imovel-hero.jpg';
-            
+            //const imagemImovel = fotoCapa?.Url || 'assets/images/imovel-hero.jpg';
+            const imagemImovel = 'https://drive.google.com/uc?export=view&id=1zpNTNo6utv0AWpkaMtlVgUnyeuENTUfK';
             console.log('IMÓVEL:', imovel.Titulo);
             console.log('FOTO:', fotoCapa);
             console.log('IMAGEM:', imagemImovel);
