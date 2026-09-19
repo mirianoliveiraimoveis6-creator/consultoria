5fa9541b0adaf198b7714b9746f6adfdc2aac059
mirianoliveiraimoveis6-creator <mirian.oliveira.imoveis6@gmail.com>
2026-09-19 20:56:45 -0300

    Update imovel.js

--- imovel.js
+++ imovel.js
@@ -432,6 +432,10 @@ carregarImovel();
 const menuButton = document.getElementById('menuButton');
 const mobileMenu = document.getElementById('mobileMenu');
 
-menuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('open');
-});
+if (menuButton && mobileMenu) {
+
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('open');
+    });
+
+}
