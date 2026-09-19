48b0344dae75815c700f42f24f76669217dc171a
mirianoliveiraimoveis6-creator <mirian.oliveira.imoveis6@gmail.com>
2026-09-19 15:43:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function formatarMoeda(valor) {
+    return Number(valor).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    });
+}
