fc1369728ee7cf9d2503ecd3326ce76f9a551d6c
mirianoliveiraimoveis6-creator <mirian.oliveira.imoveis6@gmail.com>
2026-09-20 15:15:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -441,12 +441,6 @@ async function carregarImoveis() {
 
         //carregarImoveis();
 
-        const menuButton = document.getElementById('menuButton');
-        const mobileMenu = document.getElementById('mobileMenu');
-        menuButton.addEventListener('click', () => {
-            mobileMenu.classList.toggle('open');
-        });
-
 /* =========================================================
    BUSCA DE IMÓVEIS
    ========================================================= */
